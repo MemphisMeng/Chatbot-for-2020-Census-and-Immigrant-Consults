31eed4d1ba9124f3b452c683822857b39fe8c39e
Memphis Meng <1310750076@qq.com>
2020-06-04 16:06:36 -0400

    delete tokens

--- FBMessengerChatbot/TFIDFapp.py
+++ FBMessengerChatbot/TFIDFapp.py
@@ -5,8 +5,6 @@ from FBMessengerChatbot.TFIDF.Transformer import Transformer
 import os
 
 app = Flask(__name__)
-# ACCESS_TOKEN = 'EAAlE2KkA5dYBAI6q0sW3hOFsMBGhXpHHVuLK9cQLiwjdvhXjyZC7f0enLVm7mDVe3EPP6hObCCTK4dRTZBOQrqUFyErweY9Pf04ObTaZCJvJOSPYohhoFZBQjHxvVuy7vITHgv4whpZAnfS60pU56I4kdDC1D4vcbuHmgSaZAR92BUI8NIZBEVg'
-# VERIFY_TOKEN = 'L1ZOlsiRrlBSbs/xFesH6jjkDm1OzJlwEmPa93iBNz4='
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
