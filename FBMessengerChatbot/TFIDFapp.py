e55534a3101608e806dda816278627f8671919ca
Memphis Meng <1310750076@qq.com>
2020-07-06 22:11:30 -0400

    connect to database

--- FBMessengerChatbot/TFIDFapp.py
+++ FBMessengerChatbot/TFIDFapp.py
@@ -2,21 +2,44 @@ from flask import Flask, request
 from pymessenger.bot import Bot
 from FBMessengerChatbot.TFIDF.Transformer import Transformer
 import os
+from flask_mysqldb import MySQL
+from datetime import datetime
+
+# import mysql
 
 # define on heroku settings tab
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 
-
 app = Flask(__name__)
 bot = Bot(ACCESS_TOKEN)
-transformer = Transformer('FBMessengerChatbot/data/train/QnA.csv', 'FBMessengerChatbot/data/train/SimplifiedChineseQnA.csv',
-                          'FBMessengerChatbot/data/train/traditionalChineseQnA.csv', 'FBMessengerChatbot/data/train/SpanishQnA.csv')
+transformer = Transformer('FBMessengerChatbot/data/train/QnA.csv',
+                          'FBMessengerChatbot/data/train/SimplifiedChineseQnA.csv',
+                          'FBMessengerChatbot/data/train/traditionalChineseQnA.csv',
+                          'FBMessengerChatbot/data/train/SpanishQnA.csv')
+
+# MySQL database
+app.config['MYSQL_USER'] = 'sql9353097'
+app.config['MYSQL_PASSWORD'] = 'vpwQcdCkMN'
+app.config['MYSQL_HOST'] = 'sql9.freemysqlhosting.net'
+app.config['MYSQL_DB'] = 'sql9353097'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+MYSQL_TABLE = '\'QnA\''
+
+mysql = MySQL(app)
 
 
 # We will receive messages that Facebook sends our bot at this endpoint
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
+    cur = mysql.connection.cursor()
+    # check if the database exists
+    if hasDatabase(cur) is False:
+        createDatabase(cursor=cur)
+    # check if the table exists
+    if hasTable(cur) is False:
+        createTable(cur)
+
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
         that confirms all requests that your bot receives came from Facebook."""
@@ -34,6 +57,8 @@ def receive_message():
                     # Facebook Messenger ID for user so we know where to send response back to
                     recipient_id = message['sender']['id']
                     if message['message'].get('text'):
+                        question = message['message'].get('text')
+
                         # NLP detection
                         if message['message'].get('nlp'):
                             try:
@@ -42,51 +67,99 @@ def receive_message():
                                     # greeting detected
                                     if message['message']['nlp']['entities'].get('greetings') and \
                                             message['message']['nlp']['entities']['greetings'][0]['confidence'] >= 0.9:
-                                        bot.send_text_message(recipient_id, "Hello! Nice to meet you!")
+                                        response = "Hello! Nice to meet you!"
+                                        bot.send_text_message(recipient_id, response)
+                                        time = datetime.fromtimestamp(int(str(message['timestamp'])[:-3])).strftime(
+                                            '%Y-%m-%d %H:%M:%S')
+                                        cur.execute('''INSERT INTO {} VALUES ({}, {}, {}, {})'''.format(MYSQL_TABLE,
+                                                                                                        message[
+                                                                                                            'sender'][
+                                                                                                            'id'], time,
+                                                                                                        message[
+                                                                                                            'message'].get(
+                                                                                                            'text'),
+                                                                                                        response))
                                         continue
                                     # bye detected
                                     elif message['message']['nlp']['entities'].get('bye') and \
                                             message['message']['nlp']['entities']['bye'][0]['confidence'] >= 0.9:
-                                        bot.send_text_message(recipient_id, "See you next time!")
+                                        response = "See you next time!"
+                                        bot.send_text_message(recipient_id, response)
+                                        time = datetime.fromtimestamp(int(str(message['timestamp'])[:-3])).strftime(
+                                            '%Y-%m-%d %H:%M:%S')
+                                        cur.execute('''INSERT INTO {} VALUES ({}, {}, {}, {})'''.format(MYSQL_TABLE,
+                                                                                                        message[
+                                                                                                            'sender'][
+                                                                                                            'id'], time,
+                                                                                                        message[
+                                                                                                            'message'].get(
+                                                                                                            'text'),
+                                                                                                        response))
                                         continue
                                     # thank detected
                                     elif message['message']['nlp']['entities'].get('thanks') and \
                                             message['message']['nlp']['entities']['thanks'][0]['confidence'] >= 0.9:
-                                        bot.send_text_message(recipient_id, "You are welcome!")
+                                        response = "You are welcome!"
+                                        bot.send_text_message(recipient_id, response)
+                                        time = datetime.fromtimestamp(int(str(message['timestamp'])[:-3])).strftime(
+                                            '%Y-%m-%d %H:%M:%S')
+                                        cur.execute('''INSERT INTO {} VALUES ({}, {}, {}, {})'''.format(MYSQL_TABLE,
+                                                                                                        message[
+                                                                                                            'sender'][
+                                                                                                            'id'], time,
+                                                                                                        message[
+                                                                                                            'message'].get(
+                                                                                                            'text'),
+                                                                                                        response))
                                         continue
                                 # detected Spanish
                                 elif 'es' in message['message']['nlp']['detected_locales'][0]['locale']:
                                     # greeting detected
                                     if message['message']['nlp']['entities'].get('greetings') and \
                                             message['message']['nlp']['entities']['greetings'][0]['confidence'] >= 0.6:
-                                        bot.send_text_message(recipient_id, "¡Mucho gusto! ¿Cómo estás?")
+                                        response = "¡Mucho gusto! ¿Cómo estás?"
+                                        bot.send_text_message(recipient_id, response)
+                                        time = datetime.fromtimestamp(int(str(message['timestamp'])[:-3])).strftime(
+                                            '%Y-%m-%d %H:%M:%S')
+                                        cur.execute('''INSERT INTO {} VALUES ({}, {}, {}, {})'''.format(MYSQL_TABLE,
+                                                                                                        message[
+                                                                                                            'sender'][
+                                                                                                            'id'], time,
+                                                                                                        message[
+                                                                                                            'message'].get(
+                                                                                                            'text'),
+                                                                                                        response))
                                         continue
                                     elif message['message']['nlp']['entities'].get('bye') and \
                                             message['message']['nlp']['entities']['bye'][0]['confidence'] >= 0.6:
-                                        bot.send_text_message(recipient_id, "¡Adiós!")
+                                        response = "¡adíos!"
+                                        bot.send_text_message(recipient_id, response)
+                                        time = datetime.fromtimestamp(int(str(message['timestamp'])[:-3])).strftime(
+                                            '%Y-%m-%d %H:%M:%S')
+                                        cur.execute('''INSERT INTO {} VALUES ({}, {}, {}, {})'''.format(MYSQL_TABLE,
+                                                                                                        message[
+                                                                                                            'sender'][
+                                                                                                            'id'], time,
+                                                                                                        message[
+                                                                                                            'message'].get(
+                                                                                                            'text'),
+                                                                                                        response))
                                         continue
                                     elif message['message']['nlp']['entities'].get('thanks') and \
                                             message['message']['nlp']['entities']['thanks'][0]['confidence'] >= 0.6:
-                                        bot.send_text_message(recipient_id, "¡De nada!")
+                                        response = "¡De nada!"
+                                        bot.send_text_message(recipient_id, response)
+                                        time = datetime.fromtimestamp(int(str(message['timestamp'])[:-3])).strftime(
+                                            '%Y-%m-%d %H:%M:%S')
+                                        cur.execute('''INSERT INTO {} VALUES ({}, {}, {}, {})'''.format(MYSQL_TABLE,
+                                                                                                        message[
+                                                                                                            'sender'][
+                                                                                                            'id'], time,
+                                                                                                        message[
+                                                                                                            'message'].get(
+                                                                                                            'text'),
+                                                                                                        response))
                                         continue
-                                # detected Chinese
-                                # elif 'zh' in message['message']['nlp']['detected_locales'][0]['locale']:
-                                #     # greeting detected
-                                #     if message['message']['nlp']['entities'].get('greetings') and \
-                                #             message['message']['nlp']['entities']['greetings'][0]['confidence'] >= 0.6:
-                                #         bot.send_text_message(recipient_id, "您好！很高兴为您服务！")
-                                #         print(message['message']['nlp']['entities'])
-                                #         continue
-                                #     elif message['message']['nlp']['entities'].get('bye') and \
-                                #             message['message']['nlp']['entities']['bye'][0]['confidence'] >= 0.6:
-                                #         bot.send_text_message(recipient_id, "再见！")
-                                #         print(message['message']['nlp']['entities'])
-                                #         continue
-                                #     elif message['message']['nlp']['entities'].get('thanks') and \
-                                #             message['message']['nlp']['entities']['thanks'][0]['confidence'] >= 0.6:
-                                #         bot.send_text_message(recipient_id, "不用谢！")
-                                #         print(message['message']['nlp']['entities'])
-                                #         continue
                             except KeyError:
                                 print('NLP is not deployed.')
 
@@ -101,6 +174,17 @@ def receive_message():
                             for r in responses:
                                 if r != '':
                                     bot.send_text_message(recipient_id, r.strip())
+
+                        time = datetime.fromtimestamp(int(str(message['timestamp'])[:-3])).strftime(
+                            '%Y-%m-%d %H:%M:%S')
+                        cur.execute('''INSERT INTO {} VALUES ({}, {}, {}, {})'''.format(MYSQL_TABLE,
+                                                                                        message[
+                                                                                            'sender'][
+                                                                                            'id'], time,
+                                                                                        message[
+                                                                                            'message'].get(
+                                                                                            'text'),
+                                                                                        response))
                     # if user sends us a GIF, photo,video, or any other non-text item
                     if message['message'].get('attachments'):
                         i = 0
@@ -118,5 +202,103 @@ def verify_fb_token(token_sent):
     return 'Connected sucessfully!'
 
 
+def hasDatabase(cursor):
+    try:
+        cursor.execute("""
+            SELECT SCHEMA_NAME
+            FROM INFORMATION_SCHEMA.SCHEMATA
+            WHERE SCHEMA_NAME = {}
+            """.format(app.config['MYSQL_DB']))
+        results = cursor.fetchall()
+        if results is not None:
+            return True
+        else:
+            return False
+    except Exception as err:
+        print("Something went wrong: {}".format(err))
+
+
+def createDatabase(cursor):
+    try:
+        cursor.execute("""CREATE SCHEMA {}""".format(app.config['MYSQL_DB']))
+    except Exception as err:
+        print("Something went wrong: {}".format(err))
+
+
+def hasTable(cursor):
+    try:
+        cursor.execute("""
+        SELECT COUNT(*)
+        FROM information_schema.tables
+        WHERE table_name = '{}'
+        """.format(MYSQL_TABLE))
+        results = cursor.fetchall()
+        if results is not None:
+            return True
+        else:
+            return False
+    except Exception as err:
+        print("Something went wrong: {}".format(err))
+
+
+def createTable(cursor):
+    cursor.execute(
+        '''CREATE TABLE {} (senderID VARCHAR(20), sent_time TIME, question VARCHAR(100), answer VARCHAR(100))'''.
+            format(MYSQL_TABLE))
+
+
 if __name__ == "__main__":
     app.run()
+
+{'object': 'page',
+ 'entry': [
+     {
+         'id': '100614728239050',
+         'time': 1594086355849,
+         'messaging': [
+             {
+                 'sender':
+                     {
+                         'id': '3690323734327683'
+                     },
+                 'recipient':
+                     {
+                         'id': '100614728239050'
+                     },
+                 'timestamp': 1594086355608,
+                 'message':
+                     {
+                         'mid': 'm_S0zcdsrv9gu8V0iKFaS1lULF2Q0d7p7VaJDAVF6mDIX0wVa9CGZWkWQ8ArmKki7qt0A0yvSVoos0ZnT5KtKTZw',
+                         'text': '歇息',
+                         'nlp':
+                             {
+                                 'entities':
+                                     {
+                                         'greetings': [
+                                             {
+                                                 'confidence': 0.73418509960175,
+                                                 'value': 'true'
+                                             }
+                                         ],
+                                         'location': [
+                                             {
+                                                 'suggested': True,
+                                                 'confidence': 0.34195482888008,
+                                                 'value': '歇息',
+                                                 'type': 'value'
+                                             }
+                                         ]
+                                     },
+                                 'detected_locales': [
+                                     {
+                                         'locale': 'en_XX',
+                                         'confidence': 0.4905
+                                     }
+                                 ]
+                             }
+                     }
+             }
+         ]
+     }
+ ]
+ }
